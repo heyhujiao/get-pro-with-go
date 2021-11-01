3a53b90fe3c7b5fc6568258e20a4461ef23f9869
Seng Wee <sengwee.samwell@gmail.com>
2021-11-01 11:18:25 +0800

    add iteration & update README.md

--- learn-go-with-tests/README.md
+++ learn-go-with-tests/README.md
@@ -7,3 +7,10 @@ I came across this useful Gitbook which educates new Go programmers with Go test
 https://quii.gitbook.io/learn-go-with-tests/ 
 
 Read the guide for more information!
+
+## Things to Note
+
+All the tests are placed in their individual directory because **each package must be defined in its own directory**.
+A package is a component that you can use in more than one program, that you can publish, import, get from an URL, etc. So it makes sense for it to have its own directory as much as a program can have a directory.
+
+Else, the `go test` command will not work because it will not know which package to test.
---
+++ learn-go-with-tests/iteration/repeat_test.go
@@ -0,0 +1,31 @@
+package iteration
+
+import "testing"
+
+func Repeat(character string) string {
+	var repeated string
+
+	for i := 0; i < 5; i++ {
+		repeated += character
+	}
+
+	return repeated
+}
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a")
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+// When the benchmark code is executed, it runs b.N times and measures how long it takes.
+// The amount of times the code is run shouldn't matter to you, the framework will determine what is a "good" value for that to let you have some decent results.
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
